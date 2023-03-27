8f87146b914c658e257da57d7354ad39023c55ac
R1pped <120504025+R1ppedl@users.noreply.github.com>
2023-03-27 22:25:34 +0300

    Delete Task3.java

--- Task3.java
+++
@@ -1,8 +0,0 @@
-package part1;
-
-public class Task3 {
-    public static void main(String[] args) {
-        System.out.println("Hello Belarus!");
-        System.out.println("I like Java!");
-    }
-}
