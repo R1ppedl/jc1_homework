31755daf2413e1a86dfa77c8a94711b1b282c54b
R1pped <120504025+R1ppedl@users.noreply.github.com>
2023-03-27 13:39:31 +0300

    Add files via upload

---
+++ Zadanie5.java
@@ -0,0 +1,15 @@
+public class Zadanie5 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int result = sumAndPd(a, b);
+        System.out.println("Результат: " + result);
+    }
+
+    public static int sumAndPd(int a, int b) {
+        int sum = a + b;
+        int pd = a * b;
+        int result = sum + pd;
+        return result;
+    }
+}
